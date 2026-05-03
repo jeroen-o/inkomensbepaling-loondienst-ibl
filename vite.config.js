9334cbaf947333d23dd20ba4f4a9c60e9e3e426b
jeroen-o <jeroen@oversteegen.nl>
2026-05-03 20:42:23 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ import react from '@vitejs/plugin-react';
 //
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/ibl-toetsinkomen-tool/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/inkomensbepaling-loondienst-ibl/' : '/',
   build: {
     outDir: 'dist',
     sourcemap: false,
